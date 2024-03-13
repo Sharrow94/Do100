3b42a71c00de6c5656afdf2d82ecaf60b59b2d35
Sharrow94 <maciej.roszyk@gmail.com>
2024-03-13 23:22:13 +0100

    Today only mark

--- RECE/rental/rental-domain/rental-domain-core/src/main/java/io/rece/rental/domain/core/rental/abs/offer/Offer.java
+++ RECE/rental/rental-domain/rental-domain-core/src/main/java/io/rece/rental/domain/core/rental/abs/offer/Offer.java
@@ -1,11 +1,12 @@
 package io.rece.rental.domain.core.rental.abs.offer;
 
 import io.rece.common.valueobject.id.ClientId;
-import io.rece.rental.domain.core.rental.abs.contract.Contract;
 
 public interface Offer {
 
-    Contract acceptBy(ClientId clientId);
+    void initOffer();
+
+    void accept();
 
     OfferThread startNegotiation(ClientId clientId, OfferTerms terms);
 }
