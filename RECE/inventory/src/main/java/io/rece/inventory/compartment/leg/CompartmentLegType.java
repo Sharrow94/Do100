d5e7add0beed26012b40c5288cd399e3ac9a2dcc
Sharrow94 <maciej.roszyk@gmail.com>
2024-03-23 23:46:45 +0100

    Changes based on the description of archetype pattern in the book

--- RECE/inventory/src/main/java/io/rece/inventory/compartment/leg/CompartmentLegType.java
+++ RECE/inventory/src/main/java/io/rece/inventory/compartment/leg/CompartmentLegType.java
@@ -1,11 +1,13 @@
 package io.rece.inventory.compartment.leg;
 
+import io.rece.inventory.ClassOfTravel;
 import io.rece.inventory.LegType;
 import io.rece.inventory.Station;
 import lombok.Data;
 
 @Data
 public class CompartmentLegType {
+    private ClassOfTravel classOfTravel;
     private LegType legType;
 
     public Station getOriginStation() {
--- RECE/inventory/src/main/java/io/rece/inventory/compartment/segment/CompartmentSegmentType.java
+++ RECE/inventory/src/main/java/io/rece/inventory/compartment/segment/CompartmentSegmentType.java
@@ -1,32 +1,39 @@
 package io.rece.inventory.compartment.segment;
 
-import io.rece.inventory.ClassOfTravel;
 import io.rece.inventory.LegType;
 import io.rece.inventory.Station;
+import io.rece.inventory.compartment.leg.CompartmentLegType;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class CompartmentSegmentType {
-    private ClassOfTravel classOfTravel;
     private LocalDateTime startOfPeriodOfOperation;
     private LocalDateTime endOfPeriodOfOperation;
+    private List<CompartmentLegType> compartmentLegTypes;
 
     public boolean getAvailability(LocalDateTime day) {
         return !day.isBefore(startOfPeriodOfOperation) && !day.isAfter(endOfPeriodOfOperation);
     }
 
     public Station getOriginStation() {
-        return null;
+        if (compartmentLegTypes.isEmpty()) {
+            throw new RuntimeException("CompartmentSegmentType must have origin station");
+        }
+        return compartmentLegTypes.get(0).getOriginStation();
     }
 
     public Station getDestinationStation() {
-        return null;
+        if (compartmentLegTypes.isEmpty()) {
+            throw new RuntimeException("CompartmentSegmentType must have destination station");
+        }
+        return compartmentLegTypes.get(compartmentLegTypes.size() - 1).getDestinationStation();
     }
 
     public List<LegType> getLegTypes() {
-        return null;
+        return compartmentLegTypes.stream().map(CompartmentLegType::getLegType).collect(Collectors.toList());
     }
 }
