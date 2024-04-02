631e63477629095b2efb3dd1a214961d3935085f
Sharrow94 <maciej.roszyk@gmail.com>
2024-04-02 22:40:02 +0200

    Reading about algorithms which can be of help in solving the problem of the optimal route for the passenger.

--- RECE/inventory/src/main/java/io/rece/inventory/train/TrainRoute.java
+++ RECE/inventory/src/main/java/io/rece/inventory/train/TrainRoute.java
@@ -1,10 +1,4 @@
 package io.rece.inventory.train;
 
-import io.rece.inventory.compartment.instance.CompartmentLegInstance;
-
-import java.util.List;
-
 public class TrainRoute {
-
-    List<CompartmentLegInstance> route;
 }
