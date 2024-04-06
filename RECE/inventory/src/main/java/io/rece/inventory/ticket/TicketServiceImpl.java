7538bbf0af1397e4cb152b9bf934dd3896824667
Sharrow94 <maciej.roszyk@gmail.com>
2024-04-06 23:02:42 +0200

    Reading about algorithms which can be of help in solving the problem of the optimal route for the passenger.

--- RECE/inventory/src/main/java/io/rece/inventory/ticket/TicketServiceImpl.java
+++ RECE/inventory/src/main/java/io/rece/inventory/ticket/TicketServiceImpl.java
@@ -2,20 +2,15 @@ package io.rece.inventory.ticket;
 
 import io.rece.inventory.Seat;
 import io.rece.inventory.compartment.instance.CompartmentSegmentInstance;
-import io.rece.inventory.compartment.instance.LegInstance;
 import io.rece.inventory.compartment.type.CompartmentSegmentType;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 public class TicketServiceImpl implements TicketService {
 
-    private final JourneySearchEngine journeySearchEngine;
 
     @Override
     public CompartmentSegmentInstance bookTicket(CompartmentSegmentType compartmentSegmentType, Seat seat) {
-        List<LegInstance> route = journeySearchEngine.getRoute(compartmentSegmentType.getLegTypes());
 //        return new CompartmentSegmentInstance(compartmentSegmentType, seat, route);
         return null;
     }
